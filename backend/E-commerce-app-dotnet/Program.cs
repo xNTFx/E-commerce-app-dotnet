3becfbdc4305e257f72c19bc89fd1e1bc10a20b6
xNTFVx <135262384+xNTFx@users.noreply.github.com>
2025-03-12 14:38:20 +0100

    Update Program.cs

--- backend/E-commerce-app-dotnet/Program.cs
+++ backend/E-commerce-app-dotnet/Program.cs
@@ -1,4 +1,4 @@
-﻿using E_commerce_app_dotnet;
+using E_commerce_app_dotnet;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +8,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.ConfigureServices(builder.Configuration);
 builder.Logging.AddConsole();
+
+int port = builder.Configuration.GetValue<int>("Server:Port", 8080);
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 
 var app = builder.Build();
 
